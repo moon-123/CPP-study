a777f430d57d16a5bc00cb2833f7d47d8e676e99
moon-123 <didansrl123@naver.com>
2024-04-01 22:47:12 +0900

    cpp first commit

---
+++ Day2/Day2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int num;
+    // int num1, num2; // 타입이 같으면 같이 선언 가능
+    // double num3 = 1.23, num4 = 4.56;
+
+    // num = 20;
+    
+    int a = 10;
+
+    cout << "숫자 10을 10진수로 표현하면 " << a << "이며. " << endl;
+    cout << oct;
+    cout << "숫자 10을 8진수로 표현하면 " << a << "이며, " << endl;
+    cout << hex;
+    cout << "숫자 10을 16진수로 표현하면 " << a << "입니다.";
+    
+    return 0;
+}
